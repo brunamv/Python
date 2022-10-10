e0bc754576e4f9f2065f2e6881a973825bd040af
Bruna Menegatti Vienna <99131108+brunamv@users.noreply.github.com>
2022-10-09 23:04:48 -0300

    Delete ex33.py

--- ex33.py
+++
@@ -1,8 +0,0 @@
-#erro
-sexo = input('Digite seu sexo: ')
-
-while (sexo != 'M' and sexo != 'F'):
-    print('Digite novamente M ou F!')
-    sexo = input('Digite seu sexo: ').upper()
-
-print('Fim do programa!')
